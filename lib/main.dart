90efdcd64b53e761ef6ad751c69b95860b75614c
Renat Sarymsakov <ragnor.rs@gmail.com>
2018-12-13 17:31:28 +0300

    unit test

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:github_client/main_model.dart';
 import 'package:github_client/main_component.dart';
-import 'package:github_client/repos/repo_list_presentation.dart';
 import 'package:github_client/repos/repo_component.dart';
+import 'package:github_client/repos/repo_list_presentation.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  var gitHubApi = new GitHubApi();
   runApp(
-      MyApp(
-          RepoComponent(gitHubApi)
-      )
+      MyApp(new http.Client())
   );
 }
 
 class MyApp extends StatelessWidget {
   final RepoComponent _repoComponent;
 
-  MyApp(this._repoComponent);
+  MyApp(http.Client client) : this._repoComponent = RepoComponent(client);
 
   @override
   Widget build(BuildContext context) {
--- lib/repos/repo_component.dart
+++ lib/repos/repo_component.dart
@@ -1,8 +1,9 @@
 import 'package:github_client/main_model.dart';
 import 'package:github_client/repos/repo_model.dart';
+import 'package:http/http.dart' as http;
 
 class RepoComponent {
   final RepoRepository repoRepository;
 
-  RepoComponent(GitHubApi gitHubApi) : this.repoRepository = RepoRepository(gitHubApi);
+  RepoComponent(http.Client client) : this.repoRepository = RepoRepository(GitHubApi(client));
 }
