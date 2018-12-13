54f71134b8fa50c530f058b1d9b04f22ecc8172d
Renat Sarymsakov <ragnor.rs@gmail.com>
2018-12-13 18:03:41 +0300

    integration test

---
+++ test_driver/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:github_client/main.dart' as app;
+
+void main() {
+  // This line enables the extension
+  enableFlutterDriverExtension();
+
+  app.main();
+}
